83c373697a013a72d762f4c77615f8d48000b114
fizzer <moolanp@gmail.com>
2021-10-27 21:30:14 -0700

    First Commit

---
+++ build/2019F_competition_student/adeept_awr/adeept_awr/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fizzer/ros_ws/src/2019F_competition_student/adeept_awr/adeept_awr
+# Build directory: /home/fizzer/ros_ws/build/2019F_competition_student/adeept_awr/adeept_awr
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
